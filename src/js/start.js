8e87b2520045529b091fdaffe526f1455fcde12b
Wojciech Bajko <wojciech.bajko@ngn-itsolutions.eu>
2019-12-19 15:23:04 +0100

    Change game to hangman

--- src/js/start.js
+++ src/js/start.js
@@ -1,74 +1,70 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
+    let buttonStart = document.querySelector("#startGame");
+    let newWord = document.querySelector('#newWord');
+    buttons.addEventListener("click", startGame);
 
-    let hero = {
-        name: "Tomirox",
-        stats: {
-            strength: 5,
-            agility: 5,
-            stamina: 5,
-            intellect: 5,
-            luck: 5
-        },
-        eq: {
-            helemet: {
-                val: 0,
-                name: "",
-                armor: 0,
-                bonus: 0,
-                minus: 0,
-            },
-            chest: {
-                val: 0,
-                name: "",
-                armor: 0,
-                bonus: 0,
-                minus: 0,
-            },
-            gloves: {
-                val: 0,
-                name: "",
-                armor: 0,
-                bonus: 0,
-                minus: 0,
-            },
-            legs: {
-                val: 0,
-                name: "",
-                armor: 0,
-                bonus: 0,
-                minus: 0,
-            },
-            boots: {
-                val: 0,
-                name: "",
-                armor: 0,
-                bonus: 0,
-                minus: 0,
-            },
-            weapon: {
-                damage: 0,
-                bonus: 0,
-                minus: 0
-            }
-        }
+    let word = "tekst";
+    let tab = [];
+    let i, j, k;
+    let counter = 13;
+    let tempt = 1;
 
-    }
-
-
-
-    document.querySelector("#startGame").addEventListener("click", startGame);
-    const heroName = document.querySelector("#heroName").value;
+    $(document).keydown(function(e) {
+        if (e.key === "Enter") { 
+            clearGameBoard()
+            //startGame();
+            console.log("huj");
+            
+       }
+    });
 
     function startGame() {
-        const heroName = document.querySelector("#heroName").value;
-        if(heroName){
-            hero.name = heroName;
+        document.querySelector(".conter-number").innerHTML = counter;
+        document.querySelector("#counter").className = "counter";
+        //Split word to letters
+        for (i = 0; i < word.length; i++) {
+            tab += [word.charAt(i)];
+        };
+        //Display letter in DOM
+        for (j = 0; j < tab.length; j++) {
+            document.querySelector('#word').innerHTML += '<div id="char' + j + '" class="char"></div>';
         }
-        
-        localStorage.setItem('HeroStats', JSON.stringify(hero));
-        document.querySelector('.start-container').className = "none";
-        document.querySelector('.game-container').className = "vis";
+    }
+    //Testing if keyboard pressed key is in word
+    document.onkeypress = function (evt) {
+        evt = evt || window.event;
+        let charCode = evt.keyCode || evt.which;
+        let charStr = String.fromCharCode(charCode);
+        console.log(charStr);
 
+        for (k = 0; k < tab.length; k++) {
+            if (tab[k] === charStr) {
+                document.querySelectorAll("#char").innerHTML = tab[k];
+                tempt = 1;
+            }
+            else {
+                tempt = 0;
+            }
+        }
+        if (tempt == 0) {
+            counter--;
+            document.querySelector("#counter").innerHTML = counter;
+        }
+
+    };
+
+
+
+
+
+//Clearing game board
+
+    function clearGameBoard(){
+        let removeLetters = document.querySelectorAll(".char");
+        if(removeLetters){
+            console.log(removeLetters);
+            removeLetters.parentNode.removeChild(removeLetters);
+        }
     }
 
 
